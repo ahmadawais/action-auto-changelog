6d01806908f006f89dceb8cda708a1d01dae9204
Ahmad Awais <me@AhmadAwais.com>
2020-03-21 15:02:07 +0500

    📦 NEW: Push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,34 @@
 #!/bin/sh -l
 
+set -e
+
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_TAGS=${INPUT_TAGS:-false}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+_FORCE_OPTION=''
+REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+
 npx auto-changelog
+git add .
+git commit -m " 📖 DOC: Changelog"
+
+echo "Push to branch $INPUT_BRANCH";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+if ${INPUT_FORCE}; then
+    _FORCE_OPTION='--force'
+fi
+
+if ${TAGS}; then
+    _TAGS='--tags'
+fi
+
+cd ${INPUT_DIRECTORY}
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
